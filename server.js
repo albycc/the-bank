7e4ab774d3dbe752b1458c8cbf7556910c653795
albycc <78078184+albycc@users.noreply.github.com>
2022-05-17 09:41:27 +0200

    moved ejs files to views folder
    added account navigaton to account page (index file)
    
    sass
    
    moved reset rules to reset file
    
    added basic style to account navigation menu
    
    added basic style to account container

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
 app.set('view engine', 'ejs');
-app.set('views', './public');
+app.set('views', './views');
 
 app.get('/', (req, res) =>{
     res.render('pages/index', {
